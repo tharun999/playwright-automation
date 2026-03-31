bf29a39f9aec15cc4e98b8a41ef47ddc5d920652
tharunkancherla <Tharunkancherla1999@gmail.com>
2026-03-31 17:23:24 -0600

    added before each commit message

--- Tools_QA/tests/Elements.spec.js
+++ Tools_QA/tests/Elements.spec.js
@@ -1,28 +1,35 @@
 import {test,expect} from '@playwright/test'
-
-test.beforeEach('elements clicking',async ({page})=>{
+let page;
+test.describe.serial('Elements section testing',()=>{
+test.beforeAll('elements clicking',async ({browser})=>{
+    let context= await browser.newContext();
+    page= await context.newPage();
     await page.goto('/');
     await page.locator("//*[text()='Elements']").click();
-    console.log( page.url())
-    //await expect(page).toHaveURL(process.env.DEMO_URL+'/elements')
-    await expect(page).toHaveURL(process.env.DEMO_URL+'elements');
+    await expect(page).toHaveURL(process.env.DEMO_URL +'elements');
     console.log('We have successfully entered the elements section')
 })
+test.afterAll('terminating the browser',async ({browser})=>{
+    await browser.close();
+    console.log('We have successfully closed the browser')
+})
 
-test('TextBox',async ({page})=>{
+test('TextBox',async ({})=>{
    await  page.locator("(//*[@class='menu-list'])[1]/li",{hasText:'Text Box'}).click()
    await  page.getByPlaceholder("Full Name").fill('Tharun');
    await page.fill("#userEmail","tharun@gmail.com",{timeout:process.env.TIME})
    await page.locator("#currentAddress").fill(process.env.ADDRESS)
    await page.fill("#permanentAddress",process.env.ADDRESS)
-   await page.getByRole('button',{role:'submit'}).click();
+   await page.getByRole('button', { name: 'Submit' }).click();
    await expect(page.locator("#output")).toBeVisible()
    console.log("Successfully verified the Output ")
 })
 
-test('CheckBox',async ({page})=>{
+test('CheckBox',async ({})=>{
     
-    await page.getByText('Check Box').click()
+    await page.getByText('Check Box').click();
 
     
+})
+
 })
